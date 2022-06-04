fde00c9bfda8185cb97fa77724c282cb96bb6226
Weyne <augustoweyne12@gmail.com>
2022-06-04 17:05:07 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -0,0 +1,32 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            var patio = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = "André Silva";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Verde";
+            veiculo.Modelo = "Fusca";
+            veiculo.Placa = "ASD-9999";
+
+            patio.RegistrarEntradaVeiculo(veiculo);
+            patio.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = patio.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+
+        }
+    }
+}
